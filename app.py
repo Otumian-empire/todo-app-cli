40a64870401a99ddcff5108c27f303f29318b37c
otumian-empire <popecan1000@gmail.com>
2020-10-25 18:56:54 +0000

    pass parsed_input to the Option2Parser to get command, id and task

--- app.py
+++ app.py
@@ -4,9 +4,7 @@ from parser import CoreParser, Option1Parser, Option2Parser
 if __name__ == "__main__":
 
     if len(sys.argv) > 1:
-        # TODO
-        # pass sys.argv[1:] to the Option2Parser to get command, id and task
-        option = Option2Parser()
+        option = Option2Parser(sys.argv[1:])
     else:
         option = Option1Parser()
 
